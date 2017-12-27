b0e9647ae1ff8400df3a7ad8b226763c710f163d
Ankit Tiwari <ankitt255@gmail.com>
2017-12-27 17:34:53 +0530

    📝Add comments

--- services/passport.js
+++ services/passport.js
@@ -5,14 +5,20 @@ const keys = require('../config/keys');
 const User = mongoose.model('users');
 
 passport.serializeUser((user, done) => {
+  // The second argument of done will be stored in req.session.passport.user
+  // In this case, it will be req.session.passport.user = {id: some value}
+  // The result of this function will be passed into deserializeUser
   done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
+  // The first argument is the id, which was passed by serializeUser
+  // We find the user from database and attach it
+  // to req.user
   const user = await User.findById(id);
   done(null, user);
 });
- 
+
 passport.use(
   new GoogleStrategy(
     {
