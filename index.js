160af5ec18b5a28df3759bfd18d3e365c78f4dc7
Ankit Tiwari <ankitt255@gmail.com>
2017-12-30 16:19:17 +0530

    📝 Add backend api to handle stripe payment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const keys = require('./config/keys');
 require('./models/User');
@@ -13,6 +14,8 @@ mongoose.connect(keys.mongoURI, {
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -24,6 +27,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 app.set('port', process.env.PORT || 5000);
 const server = app.listen(app.get('port'), () => {
