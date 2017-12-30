2f538a8fd5b810f5ad01599efcae8e350b2d4d83
Ankit Tiwari <ankitt255@gmail.com>
2017-12-30 15:36:45 +0530

    📝 Add an action handleToken to Payment component

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,3 +7,12 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// handleToken will be called by StripeCheckout Payment
+// component. The dispatch type FETCH_USER will be picked
+// by authReucer and it will update anything related with
+// user model, say Header
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
