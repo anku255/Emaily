08b4f7c1a3f814b8ec949bafd9ccc487876353ad
Ankit Tiwari <ankitt255@gmail.com>
2018-01-06 21:45:22 +0530

    ⚙ Pressing cancel button clears the form

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
+import { reduxForm } from 'redux-form';
 
 class SurveyNew extends Component {
   state = { showFormReview: false };
@@ -26,4 +27,9 @@ class SurveyNew extends Component {
   }
 }
 
-export default SurveyNew;
+// Adding reduxForm here assures that the
+// form gets cleared when user presses the
+// cancel button
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
