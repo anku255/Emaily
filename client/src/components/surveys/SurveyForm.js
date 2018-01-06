74d7ff260e5dd72e9dfe82f059b03cf996594e3b
Ankit Tiwari <ankitt255@gmail.com>
2018-01-06 20:39:29 +0530

    ⚙ Refactor FIELDS array into a separate file

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -4,17 +4,11 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-  { label: 'Survey Title', name: 'title' },
-  { label: 'Survey Line', name: 'subject' },
-  { label: 'Email Body', name: 'body' },
-  { label: 'Recipient List', name: 'emails' }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
   renderFields() {
-    return FIELDS.map(({ label, name }) => {
+    return formFields.map(({ label, name }) => {
       return (
         <Field
           key={name}
@@ -51,7 +45,7 @@ function validate(values) {
 
   errors.emails = validateEmails(values.emails || '');
 
-  for (let { name } of FIELDS) {
+  for (let { name } of formFields) {
     if (!values[name]) {
       errors[name] = 'You must provide a value!';
     }
