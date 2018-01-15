3cd9a1e70ae371c6ddb2d15a10a7a6453b3aa521
Ankit Tiwari <ankitt255@gmail.com>
2018-01-15 12:24:40 +0530

    ⚙ Refactor using lodash chain helper

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -41,17 +41,16 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = req.body.map(({ email, url }) => {
-      const pathname = new URL(url).pathname;
-      const match = p.test(pathname);
-      if (match) {
-        return { email, surveyId: match.surveyId, choice: match.choice };
-      }
-    });
-
-    const compactEvents = _.compact(events);
-    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
-    console.log(uniqueEvents);
+    const events = _.chain(req.body)
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+        if (match) {
+          return { email, surveyId: match.surveyId, choice: match.choice };
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .value();
     res.send({});
   });
 };
