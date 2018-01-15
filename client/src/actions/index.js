f60ff7c6bac4095363f7c49f4d7bf415cd0cbfa6
Ankit Tiwari <ankitt255@gmail.com>
2018-01-15 16:02:15 +0530

    📝 Setup Redux for surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 // using redux-thunk middleware to dispatch the action
 // only after request is complete
@@ -23,4 +23,9 @@ export const submitSurvey = (values, history) => async dispatch => {
 
   history.push('/surveys');
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get('/api/surveys');
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxForm // reduxForm reducer is provided by redux-form
+  form: reduxForm, // reduxForm reducer is provided by redux-form
+  surveys: surveysReducer
 });
