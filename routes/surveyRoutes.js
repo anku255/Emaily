834bbfa14c638d2a15c23a2b92ed5d57afc98fe1
Ankit Tiwari <ankitt255@gmail.com>
2018-01-15 12:07:31 +0530

    📝Add a survey route to handle webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,7 @@
+const _ = require('lodash');
 const mongoose = require('mongoose');
+const Path = require('path-parser');
+const { URL } = require('url');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Survey = mongoose.model('surveys');
@@ -33,5 +36,22 @@ module.exports = app => {
     } catch (err) {
       res.status(422).send(err);
     }
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+
+    const events = req.body.map(({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const match = p.test(pathname);
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice };
+      }
+    });
+
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    console.log(uniqueEvents);
+    res.send({});
   });
 };
