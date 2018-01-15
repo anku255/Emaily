2c79d86f71c04b5b042dd3d601838f6b90545e50
Ankit Tiwari <ankitt255@gmail.com>
2018-01-15 15:50:38 +0530

    📝Add a route to get the list of all surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,6 +9,13 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 module.exports = app => {
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+    res.send(surveys);
+  });
+
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!');
   });
